2d7a2baaae23df3639085e378995ac6b9e65f2cb
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2021-04-23 17:45:01 +0500

    First commit

---
+++ data/exercises-arthur-data.js
@@ -0,0 +1,31 @@
+const sentenceBanks = {
+    1: [
+        'A wild week on Wall Street ended as it began: with the retail army blowing up short bets, punishing hedge funds and minting small fortunes off the most-hated shares.',
+        'Following relentless promotion on Reddit’s WallStreetBets forum, GameStop Corp. surged 68% on Friday, taking its five-day gain to a staggering 400% as the S&P 500 Index fell 3.3%.',
+        'Shares of the video-game retailer had tumbled Thursday, when popular investment platforms including Robinhood began restricting trades. But the rally revived after the curbs were eased.',
+        'Something else that survived the week: the gaping chasm in sentiment between day-trading faithful who view GameStop as the emblem of their populist ransacking of Wall Street, and industry professionals who are convinced the bubble will burst.',
+        'You can have this euphoria go on for a while, but some day people are going to put down the phone and get back to work. Then there’s going to be too many sellers and not enough buyers of that stock.',
+    ],
+    2: [
+        "During Jackson's second term, his opponents had gradually come together to form the Whig Party. Whigs and Democrats held different attitudes toward the changes brought about by the market, banks, and commerce. The Democrats tended to view society as a continuing conflict between 'the people'-farmers, planters, and workers-and a set of greedy aristocrats. ",
+        "Whigs and Democrats differed not only in their attitudes toward the market but also about how active the central government should be in people's lives. Despite Andrew Jackson's inclination to be a strong President, Democrats as a rule believed in limited government. ",
+        'In classic research Paul Ekman took photographs of people exhibiting the emotions of anger, disgust, fear, happiness, and sadness. He then asked people around the world to indicate what emotions were being depicted in them.',
+        'As a general rule, the higher a mountain is, the more recently it was formed; for example, the high mountains of the Himalayas are only about 50 million years old.',
+        'Whatever the reason for mountain formation, as soon as land rises above sea level it is subjected to destructive forces. ',
+    ],
+    3: [
+        'Most recent laptops and all-in-one desktops have a decent — sometimes even great — built-in camera, so many people don’t need a standalone webcam.',
+        'Any webcam needs a clip that makes it simple to attach it to a variety of laptop screens and desktop monitors, and it should be easy to tilt the mic up or down to adjust the view. ',
+        'Most webcams include a noise-cancelling microphone so that you can be easily heard when you’re chatting in a room with a little ambient noise (like a ceiling fan).',
+        'All of these webcams are automatically detected by Windows 10, macOS, and other modern operating systems, but fine-tuning certain settings like the field of view is made easier with the help of this software.',
+        'In addition to the software settings, you can physically tilt the webcam up or down to control what’s in frame.',
+        'The C920S does struggle in a dimly lit room—the frame rate drops as the camera adjusts its exposure settings to keep you visible, and you’ll see more image noise and less detail.',
+    ],
+    4: [
+        'Many people spend a majority of their workdays sitting at their desks — while they work from home (especially during the pandemic) or at the office.',
+        'They also sit while driving or taking public transportation, on the couch while watching TV, and at meals.',
+        'If you’re concerned about being too sedentary or if you feel pain after sitting for long periods, you should consider switching to a standing desk.',
+        'If you’re looking to add more physical activity to your day, to lose weight, or to improve your brain power—supposed advantages that some desk manufacturers might try to sell you on—there’s no proof that a standing desk can do any of that for you. ',
+        'If you haven’t tried a standing desk before and you’re not sure you want to invest in one, you can create a makeshift version by using a cardboard box or several reams of paper to raise your monitor and keyboard.',
+    ],
+};
---
+++ js/exercises-arthur-main.js
@@ -0,0 +1,91 @@
+const shownSentence = document.querySelector('.shownSentence');
+const shownAnswer = document.querySelector('.shownAnswer');
+const startButton = document.querySelector('.startButton');
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyB9l86GgrlST_kNlUhVqLwAHXgE3Ljph8I',
+    authDomain: 'basic-chat-3f218.firebaseapp.com',
+    projectId: 'basic-chat-3f218',
+});
+
+var firestore = firebase.firestore();
+const questionsRef = firestore.doc('samples/questions');
+const answersRef = firestore.doc('samples/answers');
+
+const dataArrayUpdated = [
+    { sentence: 'question1', answer: 'yes or no?' },
+    { sentence: 'question2', answer: 'yes or no?' },
+    { sentence: 'question3', answer: 'yes or no?' },
+    { sentence: 'question4', answer: 'yes or no?' },
+    { sentence: 'question5', answer: 'yes or no?' },
+    { sentence: 'question6', answer: 'yes or no?' },
+    { sentence: 'question7', answer: 'yes or no?' },
+    { sentence: 'question8', answer: 'yes or no?' },
+    { sentence: 'question9', answer: 'yes or no?' },
+    { sentence: 'question10', answer: 'yes or no?' },
+    { sentence: 'question11', answer: 'yes or no?' },
+    { sentence: 'question12', answer: 'yes or no?' },
+    { sentence: 'question13', answer: 'yes or no?' },
+    { sentence: 'question14', answer: 'yes or no?' },
+    { sentence: 'question15', answer: 'yes or no?' },
+];
+
+const startApp = () => {
+    startButton.disabled = true;
+    dataArrayUpdated.map((element, index) => {
+        setTimeout(() => {
+            shownSentence.innerText = element.sentence;
+            shownAnswer.innerText = element.answer;
+
+            if (index === 14) {
+                startButton.disabled = false;
+            }
+        }, 2000 * index);
+    });
+};
+/* 
+const pushButton = () => {
+    dataArrayUpdated.map((element, index) => {
+        setTimeout(() => {
+            arrayIndexRef
+                .set({
+                    arrayIndex: index,
+                })
+                .then(function () {
+                    arrayIndexRef.onSnapshot(function (doc) {
+                        if (doc && doc.exists) {
+                            const myData = doc.data(); //extract the contents of the document as an object
+                            console.log(myData.arrayIndex);
+                            shownSentence.innerText =
+                                dataArrayUpdated[myData.arrayIndex].sentence;
+                        }
+                    });
+                });
+        }, 2000 * index);
+    });
+}; */
+
+const pushButton = () => {
+    startButton.disabled = true;
+    dataArrayUpdated.map((element, index) => {
+        setTimeout(() => {
+            questionsRef.onSnapshot(function (doc) {
+                if (doc && doc.exists) {
+                    const myData = doc.data(); //extract the contents of the document as an object
+                    console.log(myData[index]);
+                    shownSentence.innerText = myData[index];
+                }
+            });
+            answersRef.onSnapshot(function (doc) {
+                if (doc && doc.exists) {
+                    const myData = doc.data(); //extract the contents of the document as an object
+                    console.log(myData[index]);
+                    shownAnswer.innerText = myData[index];
+                }
+            });
+            if (index === 14) {
+                startButton.disabled = false;
+            }
+        }, 2000 * index);
+    });
+};
